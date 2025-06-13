b24e39f5a54fc3d38d071522106d8ad231a32584
Fabiano Lau Dutra <fabianolaudutra@gmail.com>
2025-06-13 12:44:02 -0300

    fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "WiseCrypto",
-        script: "index.js", // Arquivo de entrada da aplicação
+        script: "principal.js", // Arquivo de entrada da aplicação
         instances: "max", // Usar todos os núcleos da CPU
         autorestart: true, // Reiniciar automaticamente em caso de falha
         watch: false, // Não observar mudanças nos arquivos (em produção)
